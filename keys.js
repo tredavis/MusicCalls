d37c559f64413e35ed5651852c0d673ad0f73948
Montre Davis <montredavis@beck-technology.com>
2017-06-06 20:00:28 -0500

    adding more spotify stuff

---
+++ keys.js
@@ -0,0 +1,9 @@
+"user strict";
+
+const spotId = "ce833ef1272046489c5cb86af277d9c0";
+const spotSecret = "714ba0785d9b41d3828a7ad30c782d52";
+
+exports.SpotifyKeys = {
+    clientId: spotId,
+    clientSecret: spotSecret
+}
--- routes/spotify.js
+++ routes/spotify.js
@@ -3,22 +3,49 @@
 let express = require('express');
 let router = express.Router();
 
+//let's grab the spotify access keys
+let keys = require('../keys.js').SpotifyKeys;
+
+//the scopes that are availabe from spotify.
+let scopes = "playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private streaming user-follow-modify user-follow-read user-library-read user-library-modify user-read-private user-read-birthdate user-read-email user-top-read";
+let redirect_uri = "http://localhost:3000/spotify";
+
+//the spotify object
 var SpotifyWebApi = require('spotify-web-api-node');
 
 // credentials are optional
-var spotifyApi = new SpotifyWebApi({
-    clientId: 'fcecfc72172e4cd267473117a17cbd4d',
-    clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
-    redirectUri: 'http://www.example.com/callback'
-});
+// var spotifyApi = new SpotifyWebApi({
+//     clientId: keys.clientId,
+//     clientSecret: keys.clientSecret,
+//     redirectUri: 'http://localhost:3000/spotify'
+// });
 
-console.log(spotifyApi);
 
+function startAuth() {
+    console.log("about to make a call");
+    let authString = 'https://accounts.spotify.com/authorize' + '?response_type=code' + '&client_id=' + keys.SpotifyKeys + (scopes ? '&scope=' + encodeURIComponent(scopes) : '') + '&redirect_uri=' + encodeURIComponent(redirect_uri);
+
+    getRequest(authString, function(data) {
+
+        console.log("inside the spotify callback");
+        console.log(data);
+
+    });
+}
+
+function getRequest(path, callback) {
+    request.get(path, callback);
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Spotify Data Gathering Page' });
-    res.send('respond with a resource');
+router.get('spotify', function(req, res, next) {
+    console.log('yo')
+    startAuth();
+
+    // res.render('spotify', {
+    //     title: 'Spotify Data Gathering Page'
+    // });
+
 });
 
 module.exports = router;
